dfb8819cd6a921ebbe44067bc2be63ae39c54873
HendrikBD <hendrik.danen@gmail.com>
2019-06-29 22:35:14 -0400

    Adding a sidebar and defining basic layout via grid + projecting pages into layout

--- src/app/spark/content/layout/layout.module.ts
+++ src/app/spark/content/layout/layout.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TopbarComponent } from './topbar/topbar.component';
 import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     TopbarComponent,
-    LayoutComponent
+    LayoutComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule

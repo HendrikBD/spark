31a48c6f9227fdde92e1d5f69b589f55ccb3fb68
HendrikBD <hendrik.danen@gmail.com>
2019-06-30 01:12:47 -0400

    Adding some styling to the page

--- src/app/spark/content/pages/pages.component.ts
+++ src/app/spark/content/pages/pages.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'spk-pages',
   templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss'],
   styles: []
 })
 export class PagesComponent implements OnInit {

1f130aeacab35347383e540e31344e30f4082a12
HendrikBD <hendrik.danen@gmail.com>
2019-09-02 15:48:09 -0400

    Adding some initial functionality including querying projects.

--- src/app/spark/content/pages/content/projects/all-projects/all-projects.component.ts
+++ src/app/spark/content/pages/content/projects/all-projects/all-projects.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { BehaviorSubject, Subscription } from 'rxjs';
+
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'spk-all-projects',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProjectsComponent implements OnInit {
 
-  constructor() { }
+  projects = new BehaviorSubject(null);
+
+  subscriptions: {
+    projectsUpdate?: Subscription;
+  } = {};
+
+  constructor(
+    private apollo: Apollo
+  ) { }
 
   ngOnInit() {
+    this.getProjects();
+    this.subscriptions.projectsUpdate = this.projects.subscribe(this.updateDataSource.bind(this));
+  }
+
+  updateDataSource() {
+    console.log('updating data source');
+  }
+
+  getProjects() {
+    this.apollo.watchQuery<any>({
+      query: gql`
+        query getProjects {
+          projects {
+            items {
+              id
+            }
+          }
+        }
+      `
+    }).valueChanges.subscribe(res => {
+      this.projects.next(res.data.projects.items);
+    });
+  }
+
+  testKanban() {
+    this.apollo.watchQuery<any>({
+      query: gql`
+        query getKanbanCards {
+          kanbanCards {
+            items {
+              id,
+              label
+            }
+          }
+        }
+      `
+    }).valueChanges.subscribe(res => {
+      console.log(res);
+    });
   }
 
 }

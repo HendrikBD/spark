dd1d3110d3b18736862ec36af62e4c969b1133ec
HendrikBD <hendrik.danen@gmail.com>
2019-09-19 23:37:29 -0400

    Putting together an apollo server capable of subscriptions.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { createServer } from 'http';
 // import { ApolloServer, gql } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { Container } from 'typedi';
@@ -7,6 +9,7 @@ import * as path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
+import { execute, subscribe } from 'graphql';
 
 import jwt from 'express-jwt';
 
@@ -27,8 +30,11 @@ Container.set('app', app);
 // Bootstrap
 async function bootstrap() {
 
-  // Setting up playground with access token to allow access
+  const port = 4000;
   const gqlPath = '/graphql';
+  // const app = express();
+
+  // Setting up playground with access token to allow access
   app.use('/graphql', (req, res, next) => {
     req.headers.authorization = process.env.PLAYGROUND_AUTH;
     next();
@@ -43,59 +49,57 @@ async function bootstrap() {
     container: Container,
   });
 
+  // Adding useful middlewares
+  app.use('/', bodyParser.json());
+
+  // Setting up auth routes outside of gql
+  app.post(
+    '/login',
+    (Container.get('AuthService') as AuthService).login.bind(Container.get('AuthService'))
+  );
+  app.post(
+    '/create-account',
+    (Container.get('AuthService') as AuthService).createAccount.bind(Container.get('AuthService'))
+  );
+
+  // Setting up jwt check for every route except auth routes
+  app.use(
+    '*',
+    jwt({
+      secret: process.env.JWT_KEY,
+      credentialsRequired: true,
+    }).unless({
+      path: ['/create-account', '/login'],
+    })
+  );
+
+  // Test route
+  app.get('/test', (req, res) => {
+    console.log('test');
+    res.json({ test: true });
+  });
+
+
   // Starting an apollo server
   const server = new ApolloServer({
     schema,
-    // context: ({ req }) => {
-    //   const context = {
-    //     req,
-    //     user: (req as any).user.user,
-    //   };
-    //   return context;
-    // },
+    context: ({ req }) => {
+      const context = {
+        req,
+        user: (req as any).user.user,
+      };
+      return context;
+    },
     playground: true,
   });
 
-  const { url } = await server.listen(4000);
-  console.log(url)
+  // Applyies gql server as middleware(?) to app
+  server.applyMiddleware({ app, path: gqlPath });
 
-  // // Adding useful middlewares
-  // app.use('/', bodyParser.json());
-  //
-  // // Setting up auth routes outside of gql
-  // app.post(
-  //   '/login',
-  //   (Container.get('AuthService') as AuthService).login.bind(Container.get('AuthService'))
-  // );
-  // app.post(
-  //   '/create-account',
-  //   (Container.get('AuthService') as AuthService).createAccount.bind(Container.get('AuthService'))
-  // );
-  //
-  // // Setting up jwt check for every route except auth routes
-  // app.use(
-  //   '*',
-  //   jwt({
-  //     secret: process.env.JWT_KEY,
-  //     credentialsRequired: true,
-  //   }).unless({
-  //     path: ['/create-account', '/login'],
-  //   })
-  // );
-  //
-  // // Test route
-  // app.get('/test', (req, res) => {
-  //   console.log('test');
-  //   res.json({ test: true });
-  // });
-  //
-  // // Applyies gql server as middleware(?) to app
-  // server.applyMiddleware({ app, path: gqlPath });
-  //
-  // // Beginning app as server
-  // app.listen({ port: 4000 }, () => {
-  //   console.log(`Server is running, available at port: 4000`);
-  // });
+  // Beginning app as server
+  app.listen({ port: 4000 }, () => {
+    console.log(`Server is running, available at port: 4000`);
+  });
 }
 
 bootstrap().catch(console.log);

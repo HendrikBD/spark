54b5803ac8e5d50937b517e801af85027423a12a
HendrikBD <hendrik.danen@gmail.com>
2019-09-29 21:21:34 -0400

    Putting together a working express-graphql-we server that can query, mutate and subcribe to data over a websocket, that will work well with angular

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
+// import { ApolloServer, gql } from 'apollo-server-express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import { graphqlExpress } from 'apollo-server-express/dist/expressApollo';
+// import { ApolloServer, gql } from 'apollo-server';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { createServer } from 'http';
-// import { ApolloServer, gql } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { Container } from 'typedi';
 import * as path from 'path';
@@ -30,15 +32,20 @@ Container.set('app', app);
 // Bootstrap
 async function bootstrap() {
 
+
+  console.log('Initializing server...');
+
   const port = 4000;
   const gqlPath = '/graphql';
-  // const app = express();
 
   // Setting up playground with access token to allow access
-  app.use('/graphql', (req, res, next) => {
-    req.headers.authorization = process.env.PLAYGROUND_AUTH;
-    next();
-  });
+  // app.use('/graphql', (req, res, next) => {
+  //   req.headers.authorization = process.env.PLAYGROUND_AUTH;
+  //   next();
+  // });
+
+
+  console.log('Building schema...');
 
   // Building schema off of the type-graphql stuff
   const schema = await buildSchema({
@@ -48,6 +55,8 @@ async function bootstrap() {
     authChecker: authCheck,
     container: Container,
   });
+
+  console.log('Applying middlewares & auth routes...');
 
   // Adding useful middlewares
   app.use('/', bodyParser.json());
@@ -74,32 +83,71 @@ async function bootstrap() {
   );
 
   // Test route
-  app.get('/test', (req, res) => {
-    console.log('test');
-    res.json({ test: true });
-  });
+  // app.get('/test', (req, res) => {
+  //   console.log('test');
+  //   res.json({ test: true });
+  // });
 
 
   // Starting an apollo server
-  const server = new ApolloServer({
-    schema,
-    context: ({ req }) => {
-      const context = {
-        req,
-        user: (req as any).user.user,
-      };
-      return context;
-    },
-    playground: true,
-  });
+  // const server = new ApolloServer({
+  //   schema,
+  //   // context: ({ req }) => {
+  //   //   const context = {
+  //   //     req,
+  //   //     user: (req as any).user.user,
+  //   //   };
+  //   //   return context;
+  //   // },
+  //   playground: true
+  // });
+
+
+  console.log('Applying graphql schema...');
+
+  app.use('/graphql', graphqlExpress({ schema }));
+
+  console.log('Creating server...');
+  const server = createServer(app);
+
+  // server.listen({ port: 4000 });
+
+  console.log('Server created:');
+  console.log(server);
 
   // Applyies gql server as middleware(?) to app
-  server.applyMiddleware({ app, path: gqlPath });
+  // server.applyMiddleware({ app });
 
+  console.log('Listening to server...');
   // Beginning app as server
-  app.listen({ port: 4000 }, () => {
-    console.log(`Server is running, available at port: 4000`);
+  await server.listen({ port: 4000 });
+
+  console.log(`Server is running, available at: http://localhost:4000/graphql`);
+  console.log(server);
+
+  const sub = new SubscriptionServer({
+    execute,
+    subscribe,
+    schema,
+    onConnect(ctx, socket) {
+      console.log('Something connected!');
+      console.log('ctx');
+      console.log(ctx);
+      console.log('socket');
+      console.log(socket);
+    }
+  },
+    { server, path: '/graphql'}
+  );
+
+  sub.server.on('error', (ctx, socket) => {
+    console.error('Error!');
+    console.log('ctx');
+    console.log(ctx);
+    console.log('Arg 2');
+    console.log(socket);
   });
+
 }
 
 bootstrap().catch(console.log);

9484ecec14597e13cea4336de0cf025254b96196
HendrikBD <hendrik.danen@gmail.com>
2019-09-29 21:30:31 -0400

    And finally, adding a reference of how to implement queries, mutations and subscriptions via the injectable Apollo service. Front end should now be ready to handle live data!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,39 +11,73 @@ import { KanbanCard, KanbanCardQuery } from './core/types/kanbans/kanban-card.ty
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'spark';
 
-  constructor(
-    private apollo: Apollo,
-    private authService: AuthService
-  ) {}
+  constructor(private apollo: Apollo, private authService: AuthService) {}
 
   kanbanCardsQuery: Observable<KanbanCard[]>;
   kanbanCards: BehaviorSubject<KanbanCard[]> = new BehaviorSubject([]);
 
   ngOnInit() {
-    // this.kanbanCardsQuery = this.apollo.watchQuery<any>({
-    //   query: gql`
-    //     query getKanbanCards {
-    //       kanbanCards {
-    //         items {
-    //           id,
-    //           label
-    //         }
-    //       }
-    //     }
-    //   `
-    // }).valueChanges.pipe(
-    //   map(res => res.data.kanbanCards.items)
-    // );
-    //
-    // this.kanbanCardsQuery.subscribe(res => {
-    //   this.kanbanCards.next(res);
-    // });
+    this.testApollo();
   }
 
+  testApollo() {
+    this.apollo
+      .subscribe({
+        query: gql`
+          subscription subscribeToEntities {
+            subscribeToEntities {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .subscribe(res => {
+        console.log('Component successfully got subscritption data');
+        console.log(res);
+      });
 
+    this.apollo
+      .watchQuery({
+        query: gql`
+          query AllEntities {
+            getEntities {
+              id
+              name
+              type
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe(
+        res => {
+          console.log('Component successfully got static data');
+          console.log(res);
+        },
+        err => {
+          console.error(err);
+        }
+      );
+
+    setTimeout(() => {
+      console.log('updating an entity...');
+      this.apollo
+        .mutate({
+          mutation: gql`
+            mutation updateEntity {
+              updateEntity(entityUpdate: { id: 1, name: "new name", type: "does it even matter?" })
+            }
+          `,
+        })
+        .subscribe(res => {
+          console.log('Component successfully updated an entity');
+          console.log(res);
+        });
+    }, 10000);
+  }
 }
